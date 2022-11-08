43f6ae344d11ce6ad2464882644ce61754482b7b
MileLuc <melilucmel@gmail.com>
2022-11-08 10:33:24 -0300

    Subo cambios con CSS

---
+++ javascript/index.js
@@ -0,0 +1,26 @@
+let apodo = "Mile";
+
+let numeroUno = 5;
+
+const numeroDos = 10;
+
+
+
+
+
+let nombre = "Melina";
+let apellido = "Lucero";
+let edad = 35;
+let mascota = "No tengo mascota";
+
+console.log(nombre, apellido, edad, mascota)
+
+
+/* let entrada = prompt("Ingresar un mensaje");
+let salida = entrada + " " + "ingresado"; */
+/* let salida =alert(entrada + " " + "ingresado"); */
+
+/* alert(salida) */
+
+/* console.log(salida) */
+/* alert("Hola a Tod@s") */
